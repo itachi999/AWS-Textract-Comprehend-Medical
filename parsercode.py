dc87c88dddc42ac00ca77f4a277139b33d30c721
itachi999 <hari.i.hara@gmail.com>
2019-12-30 20:38:34 -0800

    Update parsercode.py

--- parsercode.py
+++ parsercode.py
@@ -1,56 +1,96 @@
-import boto3
 import json
 import sys
 import time
+import boto3
 
-textract=boto3.client('textract',aws_access_key_id='xxxxxxxxxxxxxxxxxxxx',aws_secret_access_key='xxxxxxxxxxxxxxxxxxxx',region_name='us-east-2')
 
-s3=boto3.client('s3',aws_access_key_id='xxxxxxxxxxxxxxxxxxxx',aws_secret_access_key='xxxxxxxxxxxxxxxxxxxx',region_name='us-east-2')
+#Credentials
+bucket_name = 'bucket_name'
+document_name = 'handwritten.pdf'
+aws_access_key_id = "xxxxxxxxxxxxxxxxxxxx"
+aws_secret_access_key = "xxxxxxxxxxxxxxxxxxxx"
+region_name = "us-east-2"
 
-sns=boto3.client('sns',aws_access_key_id='xxxxxxxxxxxxxxxxxxxx',aws_secret_access_key='xxxxxxxxxxxxxxxxxxxx',region_name='us-east-2')
 
-#sqs=boto3.resource('sqs',aws_access_key_id='xxxxxxxxxxxxxxxxxxxx',aws_secret_access_key='xxxxxxxxxxxxxxxxxxxx',region_name='us-east-2')
+#AWS service credentials
+topic_name = "arn:aws:sns:us-east-2:671511125968:sns_topic_name"
+lambda_name = "arn:aws:lambda:us-east-2:671511125968:function:lambda_name"
+role_name = "arn:aws:iam::671511125968:role/role_name"
+
+
+#Initializing AWS services
+textract = boto3.client('textract',aws_access_key_id = aws_access_key_id, aws_secret_access_key = aws_secret_access_key, region_name = region_name)
+
+s3 = boto3.client('s3', aws_access_key_id = aws_access_key_id, aws_secret_access_key = aws_secret_access_key, region_name = region_name)
+
+sns = boto3.client('sns', aws_access_key_id = aws_access_key_id, aws_secret_access_key = aws_secret_access_key, region_name = region_name)
+
+#sqs = boto3.resource('sqs, aws_access_key_id = aws_access_key_id, aws_secret_access_key = aws_secret_access_key, region_name = region_name)
 
 ##############################################################################################
+
 def lambda_handler(event,context):
-
-    send_message_sns_lambda=sns.subscribe(
-        TopicArn='arn:aws:sns:us-east-2:671511125968:sns_topic_name',
-        Protocol='lambda',
-        Endpoint='arn:aws:lambda:us-east-2:671511125968:function:lambda_name',
-    )
+    
+    
+    """  This lambda function is automatically triggred when a file is dropped in S3 bucket (Parameter : S3 bucket event)  """
+    
+    
+    send_message_sns_lambda = sns.subscribe(
+                                    TopicArn = topic_name,
+                                    Protocol='lambda',
+                                    Endpoint = lambda_name,
+                              )
+    
     ###########################################################################################
-    response = textract.start_document_analysis(
-        DocumentLocation={
-            'S3Object': {
-                'Bucket': 'bucket_name',
-                'Name': 'handwritten.pdf'
-            }
-        },
-        FeatureTypes=[
-            'FORMS'
-        ],
-        NotificationChannel={
-            'SNSTopicArn': 'arn:aws:sns:us-east-2:671511125968:sns_topic_name',
-            'RoleArn': 'arn:aws:iam::671511125968:role/role_name'
-        }
-    )
+
+    # Asyncronous call to Textract API
+    
+    textract_response = textract.start_document_analysis(
+        
+                        DocumentLocation={
+                            
+                                    'S3Object': {
+                                    'Bucket': bucket_name,
+                                    'Name': document_name
+                                        
+                                    }
+                        },
+        
+                        FeatureTypes=[
+                                    'FORMS'
+                                    ],
+        
+                        NotificationChannel={
+                                            'SNSTopicArn': topic_name,
+                                            'RoleArn': role_name
+                                            }
+        
+                        )
+    
     #####################################################################################################
-    ##########################################################################################################
-    x=sns.publish(
-            TopicArn='arn:aws:sns:us-east-2:671511125968:sns_topic_name', 
-            Message=json.dumps({'default': json.dumps(response)}),
-            MessageStructure='json')
+    #####################################################################################################
+    
+    #Sending Response to SNS topic
+    
+     sns_response = sns.publish(
+ 
+                            TopicArn = topic_name, 
+                            Message = json.dumps({'default': json.dumps(textract_response)}),
+                            MessageStructure = 'json'
+         
+                    )
 
+        
     #queue = sqs.get_queue_by_name(QueueName='h-sample-queue')
-
 
     #messages = queue.receive_messages(QueueUrl='https://sqs.us-east-2.amazonaws.com/h-sample-queue',MessageAttributeNames=['ALL'],MaxNumberOfMessages=10)
 
     #response2 = textract.get_document_analysis(JobId=response['JobId'],MaxResults=1000)
 
 
-    print("response: ",response)
-    print("sns publish:", x)
-    return response
+    print("response: ", textract_response)
+    print("sns publish:", sns_response)
+    
+    
+    return textract_response
 
